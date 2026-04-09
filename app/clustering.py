a7fef65ae4b5d428fd804adfe0aab868291caec2
Ashish Mehara <viratmehara232@gmail.com>
2026-04-09 19:34:35 +0530

    Delete app directory

--- app/clustering.py
+++
@@ -1,6 +0,0 @@
-import joblib
-
-cluster = joblib.load("models/cluster.pkl")
-
-def cluster_voice(features):
-    return cluster.predict(features)[0]
--- app/model.py
+++
@@ -1,6 +0,0 @@
-import joblib
-
-model = joblib.load("models/classifier.pkl")
-
-def predict_voice(features):
-    return model.predict(features)[0]
